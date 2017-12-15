5c630b0b7fd7cca029a6e4e71d99575d532b2266
harrison <harrison.a101@hotmail.com>
2017-12-15 13:28:44 -0500

    #2 Added date item component and implemented basic functionality to get file metadata from file choser.

--- src/core/portfolio/modals/add-document/add-document.ts
+++ src/core/portfolio/modals/add-document/add-document.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
 import { FileChooser } from '@ionic-native/file-chooser';
+import * as moment from 'moment';
+//import { File as NativeFile} from '@ionic-native/file';
+import { File } from '../../../data/services/file/file.service'
+
+
+declare var window;
 
 @Component({
   templateUrl: 'add-document.html'
 })
 export class AddDocumentModal {
   constructor(public viewCtrl: ViewController, private fileChooser: FileChooser) {}
+  date : String = new Date().toISOString();
+  document : File;
+
+  uri: string;
 
   dismiss() {
     this.viewCtrl.dismiss();
@@ -14,7 +24,17 @@ export class AddDocumentModal {
 
   selectFile() {
     this.fileChooser.open()
-    .then(uri => console.log(uri))
+    .then(uri => window.resolveLocalFileSystemURL(uri, (fileEntry) => {
+        fileEntry.getMetadata((metadata) => {
+            this.uri = uri;
+            this.document.size = metadata.size;
+            //this.document.path = metadata.
+            //console.log("image size : " + metadata.size);
+            //console.log("image date : " + metadata.modificationTime);
+        });
+    }))
     .catch(e => console.log(e));
   }
+
+
 }
---
+++ src/core/upload-file.service.ts
@@ -0,0 +1,10 @@
+
+export interface Document {
+    uri:string;
+    date:string;
+    type: string;
+}
+
+export class UploadFileService {
+
+}
