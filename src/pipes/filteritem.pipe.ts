a0efb3879d9e0a63c6f3bdcc10e4def559455522
l3af <cyap.mobile@gmail.com>
2017-12-16 13:11:57 -0500

    Filter by file name and format type.

--- src/pipes/filteritem.pipe.ts
+++ src/pipes/filteritem.pipe.ts
@@ -1,9 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { ItemType } from '../core/data/enum/item-type.enum';
 
-@Pipe({name: 'filterTerm'})
+@Pipe({name: 'filterItem'})
 export class FilterItemPipe implements PipeTransform {
-  transform(term: string): string {
-    console.log(term);
-    return term;
+  transform(documents: any, term: string): any {
+    const filteredDocuments = [];
+    if (!term) {
+      return documents;
+    }
+    for (const i in documents) {
+      // look at files only
+      if (documents[i].type === ItemType.FILE) {
+        // look in name and file type
+        if ((documents[i].name.toLowerCase().includes(term.toLowerCase())) ||
+            (documents[i].value.type.toLowerCase().includes(term.toLowerCase()))) {
+          filteredDocuments.push(documents[i]);
+        }
+
+      }
+    }
+    return filteredDocuments;
   }
 }
