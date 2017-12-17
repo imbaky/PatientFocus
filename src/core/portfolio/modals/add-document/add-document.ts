878df00c8f3cb0382960a75fef449929b3f150a8
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-12-17 16:03:30 -0500

    #2 add document name and selected file fields in the import modal and replace Promise with async

--- src/core/portfolio/modals/add-document/add-document.ts
+++ src/core/portfolio/modals/add-document/add-document.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController} from 'ionic-angular';
 import { FileChooser } from '@ionic-native/file-chooser';
+import { File } from '@ionic-native/file';
 import { FilePath } from '@ionic-native/file-path';
-import { FileSystemService} from "../../../data/services/file-system/file-system.service";
-import { Directory } from "../../../data/services/directory/directory.service";
-import { DocumentType } from "../../../../core/data/enum/document-type.enum";
+import { FileSystemService} from '../../../data/services/file-system/file-system.service';
+import { Directory } from '../../../data/services/directory/directory.service';
+import { DocumentType } from '../../../../core/data/enum/document-type.enum';
 
 declare var window;
 
@@ -21,9 +22,9 @@ export class AddDocumentModal {
   directory: Directory;
   date: string = new Date().toISOString();
   fullPath: string;
-  newFileName: string = new Date().toISOString(); //TODO Darrel need to add input to change file name. original file name is a number
+  documentName: string;
+  selectedFileName: string;
   selectedDocumentType: DocumentType = DocumentType.LABTEST;
-
 
   constructor(public viewCtrl: ViewController,
               private fileChooser: FileChooser,
@@ -31,33 +32,30 @@ export class AddDocumentModal {
               private fileSystemService: FileSystemService,
               private params: NavParams) {
     this.documentTypes = [
-      { name:'Lab Test', value: DocumentType.LABTEST },
-      { name:'Image Report', value: DocumentType.IMAGEREPORT },
-      { name:'Discharge Summary', value: DocumentType.DISCHARGESUMMARY },
-      { name:'Prescription', value: DocumentType.PRESCRIPTION }
+      { name: 'Lab Test', value: DocumentType.LABTEST },
+      { name: 'Image Report', value: DocumentType.IMAGEREPORT },
+      { name: 'Discharge Summary', value: DocumentType.DISCHARGESUMMARY },
+      { name: 'Prescription', value: DocumentType.PRESCRIPTION }
     ];
     this.directory = this.params.get('directory');
   }
-  
+
   dismiss() {
     this.viewCtrl.dismiss();
   }
 
-  selectFile() {
-    this.fileChooser.open()
-    .then(uri => window.resolveLocalFileSystemURL(uri, (fileEntry) => {
-        fileEntry.getMetadata((metadata) => {
-            this.filePath.resolveNativePath(uri)
-              .then(fullPath => {
-                this.fullPath = fullPath;
-              }).catch(err => console.log(err));
+  async selectFile() {
+    const uri = await this.fileChooser.open();
+    window.resolveLocalFileSystemURL(uri, (fileEntry) => {
+        fileEntry.getMetadata(async (metadata) => {
+            this.fullPath = await this.filePath.resolveNativePath(uri);
+            this.selectedFileName = this.fullPath.substring(this.fullPath.lastIndexOf('/') + 1);
         });
-    }))
-    .catch(err => console.log(err));
+    });
   }
 
-  importFile() { //TODO need import button
-    this.fileSystemService.addFile(this.fullPath, this.date, this.selectedDocumentType, this.directory);
+  importFile() {
+    this.fileSystemService.addFile(this.fullPath, this.date, this.selectedDocumentType, this.documentName, this.directory);
   }
 
 
