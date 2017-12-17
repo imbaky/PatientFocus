b1f8385317207fd7bbc7ff697b3ad365f8d862fd
harrison <harrison.a101@hotmail.com>
2017-12-17 04:38:51 -0500

    #2 Copied selected file to new application directory in phone's memory.

--- src/core/data/services/file-system/file-system.service.ts
+++ src/core/data/services/file-system/file-system.service.ts
@@ -1,28 +1,39 @@
 import { Injectable } from '@angular/core';
 
-import { Directory } from '../directory/directory.service';
+import {Directory, DirectoryService} from '../directory/directory.service';
 import { Item } from '../item/item.service';
-import { File } from '../file/file.service'
-import {ItemType} from "../../enum/item-type.enum";
+import { File } from '@ionic-native/file';
+
+declare var window;
 
 @Injectable()
 export class FileSystemService {
 
+  constructor(
+    private directoryService: DirectoryService,
+    private file : File
+  ) { }
+
   /**
    * Adds a file to a directory
-   * @param file
-   * @param directory
+   * @param fullPath path of where the original file is located
+   * @param Date in which the document should be placed
+   * @param directory Profile directory
    */
-  addFile(file: File, directory: Directory) {
-    var item: Item =  {
-      name: "name",
-      description: "description",
-      type: ItemType.FILE,
-      type_id: 1,
-      created: "date",
-      value: file
-    };
-    directory.items.push(item);
+  addFile(fullPath: string, creationDate: string, type: string, directory: Directory) {
+      console.log(fullPath);
+      var filename = fullPath.substring(fullPath.lastIndexOf('/')+1); //get file name
+      console.log(filename);
+      var url = fullPath.substring(0, fullPath.lastIndexOf("/")); //remove filename from path
+      console.log(url);
+      //this.file.copyFile(url, filename, this.file.dataDirectory+"Documents/" + directory.id + "/", ""). //TODO need to add directory to phone
+      this.file.copyFile(url, filename, this.file.dataDirectory+"Documents/", "").
+      then( (entry) => {
+        this.directoryService.addFileToDirectory(entry, creationDate, type, directory);
+        this.file.listDir(this.file.dataDirectory, "Documents")
+          .then( entrties => console.log(entrties))
+          .catch( err => console.log("Could not list files"))
+      }).catch(err => console.log(err));
   }
 
   /**
