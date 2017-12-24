cd77a798cad4424cec857ee69d8f221db143da4c
harrison <harrison.a101@hotmail.com>
2017-12-23 19:38:17 -0500

    Merge branch 'feature/#2-import-file-rebased' of https://github.com/imbaky/PatientFocus into feature/#2-import-file-rebased

--- src/core/data/enum/document-type.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum DocumentType {
-  LABTEST = 'Lab Test',
-  IMAGEREPORT = 'Image Report',
-  PRESCRIPTION = 'Prescription',
-  DISCHARGESUMMARY = 'Discharge Summary',
-  CONSULTATIONREPORT = 'Consultation Report'
-}
