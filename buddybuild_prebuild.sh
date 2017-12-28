79b7aa81257b36b90c0364b800a66ce2f15131af
imbaky <mo7_abd@hotmail.com>
2017-12-28 12:22:01 -0500

    Added ionic and cordova as part of the npm package
    moved adding platform to postinstall

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# Add android platform
-ionic cordova platform add android --no-interactive --confirm 
-
 # fixes the cordova conceal gradle before buddybuild builds the apk
 sed -i 's/1.0.0@aar/2.0.1@aar/g' /tmp/sandbox/workspace/platforms/android/cordova-safe/starter-conceal.gradle
