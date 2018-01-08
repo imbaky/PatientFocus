e29d735d2e5192c451350485498a71ff26673fce
harrison <harrison.a101@hotmail.com>
2018-01-07 20:49:08 -0500

    #29 There are now 2 names for a file, the user defined name, and the actual file name.

--- src/core/data/services/dexie/database.ts
+++ src/core/data/services/dexie/database.ts
@@ -2,6 +2,6 @@ export const SCHEMA = {
   'directory': '++id',
   'profile': '++id, directory, name, password',
   'item': '++id, name, description, type, type_id, directory_id, created',
-  'file': '++id, path, size, type'
+  'file': '++id, path, size, type, user_defined_name'
 };
 
