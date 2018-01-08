4ef6203e777e67cc23ed567faba1ba3ec864892f
harrison <harrison.a101@hotmail.com>
2018-01-07 20:50:20 -0500

    #41 Removed fake documents from dexie.

--- src/core/data/services/dexie/dexie.service.ts
+++ src/core/data/services/dexie/dexie.service.ts
@@ -20,52 +20,6 @@ export class DexieService extends Dexie {
       this.table('directory').bulkAdd([
         { id: 1 }
       ]);
-      this.table('item').bulkAdd([
-        {
-          name: 'Filename1.txt',
-          description: 'lab test1',
-          type: ItemType.FILE,
-          type_id: 1,
-          directory_id: 1,
-          chosen_date: date.toISOString()
-        },
-        {
-          name: 'Filename2.txt',
-          description: 'lab test2',
-          type: ItemType.FILE,
-          type_id: 2,
-          directory_id: 1,
-          chosen_date: date.toISOString()
-        },
-        {
-          name: 'Filename2.txt',
-          description: 'blood test',
-          type: ItemType.FILE,
-          type_id: 3,
-          directory_id: 1,
-          chosen_date: date.toISOString()
-        },
-      ]);
-      this.table('file').bulkAdd([
-        {
-          path: '::directory/subdirectory/subsubdirectory',
-          size: 4576543,
-          document_type: DocumentType.BLOOD_TEST,
-          format: FileFormatType.JPG
-        },
-        {
-          path: '::directory/subdirectory/subsubdirectory',
-          size: 245364,
-          document_type: DocumentType.CONSULTATION,
-          format: FileFormatType.PNG
-        },
-        {
-          path: '::directory/subdirectory/subsubdirectory',
-          size: 34564,
-          document_type: DocumentType.PRESCRIPTION,
-          format: FileFormatType.PDF
-        }
-      ]);
     });
   }
 }
