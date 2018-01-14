7bf81c0856f8d528f1b61d5bac7f4a1d46cf125d
harrison <harrison.a101@hotmail.com>
2018-01-14 04:07:34 -0500

    #54 Moved filter location to pipe directory and fixed bug related to newly importing files not being displayed due to date filter.

--- src/pipes/file-filter/file-filter.pipe.ts
+++ src/pipes/file-filter/file-filter.pipe.ts
@@ -1,18 +1,79 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ItemType } from '../../core/data/enum/item-type.enum';
+import * as moment from 'moment';
 
 import { Item } from '../../core/data/services/item/item.service';
+import {File} from '../../core/data/services/file/file.service';
+import {DocumentType, FileFormatType} from '../../core/data/enum/file-type.enum';
+
 
 @Pipe({
-  name: 'fileFilter'
+  name: 'fileFilterByDate'
 })
-export class FileFilter implements PipeTransform {
-  transform(items: Item[], filterFunc: Function, term: any): any {
-
-    if (!term || !items) {
+export class FileFilterByDate implements PipeTransform {
+  transform(items: Item[], dateFromTerm: string, dateToTerm: string) {
+    if (!items) {
       return items;
     }
+    return items.filter((item) => {
+      if (item.type === ItemType.DIRECTORY) {
+        return false;
+      }
+      // need to specify granularity because chosen_date has a precision of miliseconds, so the granularity needs to be
+      // reduced to days
+      const isBetween = (moment(item.chosen_date).isBetween(dateFromTerm, dateToTerm, 'day', '[]'));
+      return isBetween;
+    });
+  }
+}
 
-    return items.filter((item) => item.type === ItemType.FILE && filterFunc(item, term));
+@Pipe({
+  name: 'fileFilterByName'
+})
+export class FileFilterByName implements PipeTransform {
+  transform(items: Item[], filterName: string) {
+    if (!items) {
+      return items;
+    }
+    return items.filter((item) => {
+      if (item.type === ItemType.DIRECTORY) {
+        return false;
+      }
+       return (item.value as File).user_defined_name.toLowerCase().includes(filterName.toLowerCase());
+    });
+  }
+}
+
+@Pipe({
+  name: 'fileFilterByDocType'
+})
+export class FileFilterByDocType implements PipeTransform {
+  transform(items: Item[], filterDoctype: DocumentType) {
+    if (!filterDoctype || !items) {
+      return items;
+    }
+    return items.filter((item) => {
+      if (item.type === ItemType.DIRECTORY) {
+        return false;
+      }
+      return (item.value as File).document_type === filterDoctype;
+    });
+  }
+}
+
+@Pipe({
+  name: 'fileFilterByFormatType'
+})
+export class FileFilterByFormatType implements PipeTransform {
+  transform(items: Item[], format: FileFormatType) {
+    if (!format || !items) {
+      return items;
+    }
+    return items.filter((item) => {
+      if (item.type === ItemType.DIRECTORY) {
+        return false;
+      }
+      return (item.value as File).format === format;
+    });
   }
 }
