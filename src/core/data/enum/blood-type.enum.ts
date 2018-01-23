278ccd723d477e68b475e9f1fb2f7eb6d6a00389
McGiver- <gkolampas@gmail.com>
2018-01-23 14:50:03 -0500

    #13 added medical-info service

---
+++ src/core/data/enum/blood-type.enum.ts
@@ -0,0 +1,10 @@
+export enum BloodType {
+    A_NEG  = 'A-',
+    A_POS  = 'A+',
+    B_NEG  = 'B-',
+    B_POS  = 'B+',
+    O_NEG  = 'O-',
+    O_POS  = 'O+',
+    AB_NEG = 'AB-',
+    AB_POS = 'AB+',
+}
