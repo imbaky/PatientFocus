235a5b33dffd45eba17317368b03b71ba13a78d2
McGiver- <gkolampas@gmail.com>
2018-02-07 22:29:41 -0500

    exploring with broadcast listener

--- src/core/data/services/serve-files/serve-files.service.ts
+++ src/core/data/services/serve-files/serve-files.service.ts
@@ -4,7 +4,8 @@ import { UUID } from 'angular2-uuid';
 
 import {ProfileService, UserProfile} from '@services/profile/profile.service';
 import {MedicalInfoService, MedicalInfo} from '@services/medical-info/medical-info.service';
-
+import { Broadcaster } from '@ionic-native/broadcaster';
+import {Httpd,HttpdOptions} from '@ionic-native/httpd';
 
 @Injectable()
 export class ServeFilesService {
@@ -17,9 +18,25 @@ export class ServeFilesService {
 
     constructor(
         private dexie: DexieService,
+        private broadcaster: Broadcaster,
+        private httpd: Httpd,
         private profile: ProfileService,
         private medical: MedicalInfoService
     ) {
-        this.webextension = UUID.UUID();
+        this.broadcaster.addEventListener('shareFiles')
+            .subscribe((event) => {
+                console.log(event);
+                this.webextension = UUID.UUID();
+                const options: HttpdOptions = {
+                    www_root: 'sharefolder',
+                    port: 8080,
+                    localhost_only: false
+                };
+
+                this.httpd.startServer(options).subscribe((data) => {
+                    document.getElementById('url').innerHTML = 'server is started: <a href=' + data + '>' + data + '</a>';
+                });
+
+            });
     }
 }
