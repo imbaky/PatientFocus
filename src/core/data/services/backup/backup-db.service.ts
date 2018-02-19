77ce51d9960b79e726dc0b006bb81867ce8a6510
harrison <harrison.a101@hotmail.com>
2018-02-19 13:56:19 -0500

    #4 added docs on export and import profile

--- src/core/data/services/backup/backup-db.service.ts
+++ src/core/data/services/backup/backup-db.service.ts
@@ -16,6 +16,14 @@ export class BackupDBService {
               private nativeZip: Zip,
               private dexieService: DexieService) {}
 
+  /**
+   *  Allows to convert the database into an encrypted json string which will then be stored onto a file called
+   *  database.txt. That file will then be placed in a zip file. Additionally, all files of a profile will be
+   *  saved onto the same zip file. The zip file will be stored onto the downloads directory of the phone
+    * @param {number} profile_id The profile id to be exported
+   * @param {string} password The password of the user which will be used as the key to encrypt the database data
+   * @returns {Promise<string>} Returns the unencrypted json string
+   */
   async exportProfile(profile_id: number, password: string) {
     let jsonTables = {};
     let tables = this.dexieService.tables;
@@ -25,7 +33,7 @@ export class BackupDBService {
       jsonTables[String(table.name)] = jsonString;
     }
     let jsonString = JSON.stringify(jsonTables);
-    let encrpyted = crypto.AES.encrypt(jsonString, 'key');
+    let encrpyted = crypto.AES.encrypt(jsonString, password);
     try {
       let zip = new jszip();
       let path = this.file.externalDataDirectory + String(profile_id) +"/";
@@ -48,23 +56,28 @@ export class BackupDBService {
     return jsonString;
   }
 
-  async importTable() { // TODO used to import profile incomplete
+  /**
+   * Imports the zip file created by exportProfile which contains the profile of the user. The zip file should contain a
+   * file 'database.txt' in the root of the zip file. The database.txt file will be encrypted, so to decrypt the file,
+   * the method requires the users password, which was used as the key to encrypt the file. Additionally, this method will
+   * unzip all the files of the profile to the correct directory on the phone.
+   * @param {string} password The password used to decrypt the profile
+   * @returns {Promise<any>} returns the decryptedData database
+   */
+
+  async importProfile(password: string) { // TODO used to import profile incomplete
     try {
       let pathToZipFile = this.file.externalDataDirectory + String(1) +"/" + this.zipFile; // TODO need proper path
       let pathToDestination = this.file.externalDataDirectory + String(1);
       //let pathToFile = this.file.externalDataDirectory + String(1); // TODO need proper path
       //let pathToUnZipped = this.rootPath + String(1); // TODO url needs to be changed
-      console.log(pathToZipFile);
       let number = await this.nativeZip.unzip(pathToZipFile, pathToDestination); // 1.
-      console.log(number);
       /*let number = await this.nativeZip.unzip("test.zip", pathToUnZipped, () => {
-        console.log("inside");
       });// 2.*/
       let encrypyedJson = await this.file.readAsText(pathToDestination, this.fileName);
       console.log("encrypyedJson", encrypyedJson);
       //let encrypyedJson = await this.file.readAsText(this.rootPath, this.fileName);
-      var bytes = crypto.AES.decrypt(encrypyedJson.toString(), 'key');
-      //console.log("bytes", bytes);
+      var bytes = crypto.AES.decrypt(encrypyedJson.toString(), password);
       var decryptedData = JSON.parse(bytes.toString(crypto.enc.Utf8));
     }
     catch(e) {
