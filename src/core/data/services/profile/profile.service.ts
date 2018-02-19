61a585bc58f5d63f9848ecfae35c4b9f8d1cbe06
harrison <harrison.a101@hotmail.com>
2018-02-19 03:36:11 -0500

    #4 Created directory in memory for new profile for files to be stored

--- src/core/data/services/profile/profile.service.ts
+++ src/core/data/services/profile/profile.service.ts
@@ -5,6 +5,8 @@ import Dexie from 'dexie';
 import { DirectoryService } from '../directory/directory.service';
 import {Item, ItemService} from '../item/item.service';
 import { Events } from 'ionic-angular';
+import { FileSystemService } from "@services/file-system/file-system.service";
+import { BackupDBService } from '@services/backup/backup-db.service';
 
 
 export interface UserProfile {
@@ -25,7 +27,9 @@ export class ProfileService {
     private dexie: DexieService,
     private directoryService: DirectoryService,
     private itemService: ItemService,
-    private events: Events
+    private events: Events,
+    private fileSystemService: FileSystemService,
+    private backUpDBService: BackupDBService
   ) {
     this.table = this.dexie.table('profile');
   }
@@ -46,9 +50,7 @@ export class ProfileService {
       password: profile.password
     };
     const profileId = await this.table.put(newProfile);
-    const directoryId = await this.directoryService.createNewDirectory(
-      profileId
-    );
+    const directoryId = await this.fileSystemService.createNewDirectory(profileId);
     newProfile = await this.table.get(profileId);
     newProfile.directory = directoryId;
     return await this.table.put(newProfile);
@@ -96,6 +98,11 @@ export class ProfileService {
    */
   getProfileDiaryItems(profileId: number): Promise<Item[]> {
     return this.itemService.getDiaryItemsByProfileID(profileId);
+  }
+
+  async exportProfile(profile_id: number) {
+    let profile = await this.getFirstProfile();
+    this.backUpDBService.exportProfile(profile_id, profile.password);
   }
 }
 
