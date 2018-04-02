2f2be116ce7fcd1d629e1fb21a2cfd764ac9eab4
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-04-02 12:30:18 -0400

    #113 confirm password on account creation

--- src/validators/password-validator.ts
+++ src/validators/password-validator.ts
@@ -3,8 +3,6 @@ export class PasswordValidator {
   static MatchPassword(Control: AbstractControl) {
     const password = Control.get('password').value;
     const confirmPassword = Control.get('passwordConf').value;
-    console.log(password);
-    console.log(confirmPassword);
     if (password != confirmPassword) {
       Control.get('passwordConf').setErrors({ MatchPassword: true });
     } else {
