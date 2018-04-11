2e5b325afe7d3cd6bb6ebeb161b287d9b48e1eac
Christopher Yap <cyap.mobile@gmail.com>
2018-04-11 19:40:02 -0400

    #116 Fix test that counted pages.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -56,8 +56,8 @@ describe('MyApp Component', () => {
         expect(component instanceof MyApp).toBe(true);
     });
 
-    it('should have four pages', () => {
-        expect(component.pages.length).toBe(4);
+    it('should have five pages', () => {
+        expect(component.pages.length).toBe(5);
     });
 
 });
