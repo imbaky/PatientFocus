dcc472907f05b298e0f950ed3c082c2abefee218
Christopher Yap <cyap.mobile@gmail.com>
2018-04-10 20:22:32 -0400

    #116 Added scrolling to navigate quicker per suggestions

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Content } from 'ionic-angular';
 
 @Component({
     selector: 'page-help',
     templateUrl: 'help.html'
 })
 export class HelpPage {
+    @ViewChild(Content) content: Content;
+
+    scrollToTop() {
+        this.content.scrollToTop();
+    }
+
+    scrollTo(element:string) {
+        let yOffset = document.getElementById(element).offsetTop;
+        this.content.scrollTo(0, yOffset, 4000)
+    }
 }
